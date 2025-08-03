33927ce01f6184dab8187b66bf15ca3d46f72a70
LeeHyeonjun <little6523@naver.com>
2025-08-03 16:43:03 +0900

    프로젝트 최초 생성

---
+++ src/test/java/com/devhyeon/MyScheduler/MySchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhyeon.MyScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
