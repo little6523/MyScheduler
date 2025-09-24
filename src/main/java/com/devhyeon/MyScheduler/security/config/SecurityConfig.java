019ee00e1d8d298f80ba4b75076f572532da3fa0
LeeHyeonjun <little6523@naver.com>
2025-09-25 00:14:16 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/devhyeon/MyScheduler/security/config/SecurityConfig.java
+++ src/main/java/com/devhyeon/MyScheduler/security/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
