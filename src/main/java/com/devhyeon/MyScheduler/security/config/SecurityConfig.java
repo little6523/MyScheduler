15b8b6acaf25ca41258eecd08f58fb622d39b725
LeeHyeonjun <little6523@naver.com>
2025-11-22 23:08:45 +0900

    feat: 시큐리티에서 메인 페이지 바로 접속되도록 허용 및 메인 화면 반환 메소드 이름 변경

--- src/main/java/com/devhyeon/MyScheduler/security/config/SecurityConfig.java
+++ src/main/java/com/devhyeon/MyScheduler/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ public class SecurityConfig {
           "/",
           "/myscheduler/login",
           "/myscheduler/signup",
+          "/myscheduler",
           "/api/signup",
           "/api/login",
           "/css/**",
